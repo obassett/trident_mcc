d67016e4ac657d4aa018c91e8531de5c0d64ae9c
Ollie <oliver@bassett.net.nz>
2021-07-06 07:10:46 +0000

    Startup script now listens all interfaces.

--- trident-mcc/trident_mcc.sh
+++ trident-mcc/trident_mcc.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-poetry run uvicorn trident_mcc.healthz:app --log-config trident_mcc/logging.yaml &
+poetry run uvicorn trident_mcc.healthz:app --host 0.0.0.0 --log-config trident_mcc/logging.yaml &
 poetry run python3 -m trident_mcc
